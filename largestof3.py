674574eb992adfba5d87d46c6ee402ee9e9032c9
del1311 <del1311x@gmail.com>
2019-03-05 15:28:54 -0800

    greattt

---
+++ largestof3.py
@@ -0,0 +1,13 @@
+a=676767
+b=867687
+c=5746
+if(a>b):
+    if(a>c):    
+        print(a)
+    else:
+        print(c)
+else:
+    if(b>c):
+        print(b)
+    else:
+         print(c)                   
